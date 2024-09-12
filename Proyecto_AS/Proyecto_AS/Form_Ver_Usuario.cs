ed0d3f6b988f31ead0092055811e503a5c0012ba
Nicolas <nicolas.olivares.geisser@gmail.com>
2024-09-12 02:32:58 -0300

    inclui mi base de datos a los archivos

--- Proyecto_AS/Proyecto_AS/Form_Ver_Usuario.cs
+++ Proyecto_AS/Proyecto_AS/Form_Ver_Usuario.cs
@@ -14,7 +14,8 @@ namespace Proyecto_AS
     public partial class Form_Ver_Usuario : Form
     {
         //Creamos un string el cual contendra los datos para necesario para poder conectarse a la bd
-        static string inicio_sesion = "Server=PAOLO\\SQLEXPRESS ;Database=BD_AS ;User id=sa ;Password=12345678;";
+        static string inicio_sesion = "Server=LAPTOP-9H0B86NU ;Database=BD_AS ;User id=sa ;Password=697400naxo;";
+        //static string inicio_sesion = "Server=PAOLO\\SQLEXPRESS ;Database=BD_AS ;User id=sa ;Password=12345678;";
         SqlConnection conectar = new SqlConnection(inicio_sesion); /*asignamos el comando para la conexion*/
         public Form_Ver_Usuario()
         {
