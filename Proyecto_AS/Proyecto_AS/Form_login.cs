5dd5b2d913f660a2ba0416f56a804101d8a1e2b9
Nicolas <nicolas.olivares.geisser@gmail.com>
2024-09-12 01:11:17 -0300

    prueba 3

--- Proyecto_AS/Proyecto_AS/Form_login.cs
+++ Proyecto_AS/Proyecto_AS/Form_login.cs
@@ -15,6 +15,7 @@ namespace Proyecto_AS
     {
         //prueba 
         //prueba2
+        //prueba 3
         //Creamos un string el cual contendra los datos para necesario para poder conectarse a la bd
         static string inicio_sesion = "Server=LAPTOP-9H0B86NU ;Database=BD_AS ;User id=sa ;Password=697400naxo;";
         SqlConnection conectar = new SqlConnection(inicio_sesion); /*asignamos el comando para la coneccion*/
