3728866b86c652fb2a99715c48308970e484dbe4
mayerk2 <mayerk.paolo@gmail.com>
2024-09-12 01:15:04 -0300

    prueba4

--- Proyecto_AS/Proyecto_AS/Form_login.cs
+++ Proyecto_AS/Proyecto_AS/Form_login.cs
@@ -18,6 +18,7 @@ namespace Proyecto_AS
         //prueba 3
         //Creamos un string el cual contendra los datos para necesario para poder conectarse a la bd
         static string inicio_sesion = "Server=LAPTOP-9H0B86NU ;Database=BD_AS ;User id=sa ;Password=697400naxo;";
+        //static string inicio_sesion ="Server=PAOLO\SQLEXPRESS ;Database=BD_AS ;User id=sa ;Password=12345678;";
         SqlConnection conectar = new SqlConnection(inicio_sesion); /*asignamos el comando para la coneccion*/
         public Form_login()
         {
