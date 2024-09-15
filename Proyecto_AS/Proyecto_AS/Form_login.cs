1a7f82169d08c590902b3476017bcaede0ac1867
Nicolas <nicolas.olivares.geisser@gmail.com>
2024-09-15 17:32:55 -0300

    enseñando a mati

--- Proyecto_AS/Proyecto_AS/Form_login.cs
+++ Proyecto_AS/Proyecto_AS/Form_login.cs
@@ -13,9 +13,7 @@ namespace Proyecto_AS
 {
     public partial class Form_login : Form
     {
-        //prueba 
-        //prueba2
-        //prueba 3
+        //wena mati
         //Creamos un string el cual contendra los datos para necesario para poder conectarse a la bd
         static string inicio_sesion = "Server=LAPTOP-9H0B86NU ;Database=BD_AS ;User id=sa ;Password=697400naxo;";
         //static string inicio_sesion = "Server=PAOLO\\SQLEXPRESS ;Database=BD_AS ;User id=sa ;Password=12345678;";
