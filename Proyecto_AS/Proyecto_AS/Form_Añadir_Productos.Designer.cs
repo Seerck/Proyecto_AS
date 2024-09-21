138442ebe46b3c95d9ef2ac75f2aad2bca99c5ce
math <matiassilvac32@gmail.com>
2024-09-21 17:08:03 -0300

    pude arreglar los problemas principales me falta mejorar mas aspectos del programa pero cumple con lo solicitado

--- Proyecto_AS/Proyecto_AS/Form_Añadir_Productos.Designer.cs
+++ Proyecto_AS/Proyecto_AS/Form_Añadir_Productos.Designer.cs
@@ -31,7 +31,6 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.nombrecmd = new System.Windows.Forms.TextBox();
             this.tipocmd = new System.Windows.Forms.TextBox();
-            this.cantidadcmd = new System.Windows.Forms.TextBox();
             this.ubicacioncmd = new System.Windows.Forms.TextBox();
             this.fechaingresocmd = new System.Windows.Forms.TextBox();
             this.fechasalidacmd = new System.Windows.Forms.TextBox();
@@ -44,6 +43,11 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.estantecmd = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_cantidad = new System.Windows.Forms.TextBox();
+            this.txt_precio = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,62 +61,55 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(901, 335);
+            this.dataGridView1.Size = new System.Drawing.Size(884, 335);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // nombrecmd
             // 
-            this.nombrecmd.Location = new System.Drawing.Point(12, 384);
+            this.nombrecmd.Location = new System.Drawing.Point(12, 381);
             this.nombrecmd.Name = "nombrecmd";
             this.nombrecmd.Size = new System.Drawing.Size(261, 22);
             this.nombrecmd.TabIndex = 2;
             // 
             // tipocmd
             // 
-            this.tipocmd.Location = new System.Drawing.Point(12, 445);
+            this.tipocmd.Location = new System.Drawing.Point(12, 425);
             this.tipocmd.Name = "tipocmd";
             this.tipocmd.Size = new System.Drawing.Size(261, 22);
             this.tipocmd.TabIndex = 3;
             // 
-            // cantidadcmd
-            // 
-            this.cantidadcmd.Location = new System.Drawing.Point(9, 504);
-            this.cantidadcmd.Name = "cantidadcmd";
-            this.cantidadcmd.Size = new System.Drawing.Size(261, 22);
-            this.cantidadcmd.TabIndex = 4;
-            // 
             // ubicacioncmd
             // 
-            this.ubicacioncmd.Location = new System.Drawing.Point(325, 384);
+            this.ubicacioncmd.Location = new System.Drawing.Point(311, 381);
             this.ubicacioncmd.Name = "ubicacioncmd";
             this.ubicacioncmd.Size = new System.Drawing.Size(261, 22);
             this.ubicacioncmd.TabIndex = 5;
             // 
             // fechaingresocmd
             // 
-            this.fechaingresocmd.Location = new System.Drawing.Point(325, 445);
+            this.fechaingresocmd.Location = new System.Drawing.Point(311, 425);
             this.fechaingresocmd.Name = "fechaingresocmd";
             this.fechaingresocmd.Size = new System.Drawing.Size(261, 22);
             this.fechaingresocmd.TabIndex = 6;
             // 
             // fechasalidacmd
             // 
-            this.fechasalidacmd.Location = new System.Drawing.Point(325, 504);
+            this.fechasalidacmd.Location = new System.Drawing.Point(311, 470);
             this.fechasalidacmd.Name = "fechasalidacmd";
             this.fechasalidacmd.Size = new System.Drawing.Size(261, 22);
             this.fechasalidacmd.TabIndex = 7;
             // 
             // estadocmd
             // 
-            this.estadocmd.Location = new System.Drawing.Point(615, 384);
+            this.estadocmd.Location = new System.Drawing.Point(311, 518);
             this.estadocmd.Name = "estadocmd";
             this.estadocmd.Size = new System.Drawing.Size(261, 22);
             this.estadocmd.TabIndex = 8;
             // 
             // btnañadir
             // 
-            this.btnañadir.Location = new System.Drawing.Point(638, 445);
+            this.btnañadir.Location = new System.Drawing.Point(623, 461);
             this.btnañadir.Name = "btnañadir";
             this.btnañadir.Size = new System.Drawing.Size(218, 70);
             this.btnañadir.TabIndex = 9;
@@ -132,7 +129,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 423);
+            this.label2.Location = new System.Drawing.Point(12, 406);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 16);
             this.label2.TabIndex = 11;
@@ -141,7 +138,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 485);
+            this.label3.Location = new System.Drawing.Point(12, 451);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 16);
             this.label3.TabIndex = 12;
@@ -150,7 +147,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(322, 366);
+            this.label4.Location = new System.Drawing.Point(308, 362);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(68, 16);
             this.label4.TabIndex = 13;
@@ -159,7 +156,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(322, 423);
+            this.label5.Location = new System.Drawing.Point(308, 406);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(93, 16);
             this.label5.TabIndex = 14;
@@ -168,7 +165,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(328, 485);
+            this.label6.Location = new System.Drawing.Point(314, 451);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(87, 16);
             this.label6.TabIndex = 15;
@@ -177,18 +174,63 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(619, 366);
+            this.label7.Location = new System.Drawing.Point(308, 499);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(50, 16);
             this.label7.TabIndex = 16;
             this.label7.Text = "Estado";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(610, 362);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 16);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Nivel Estante";
+            // 
+            // estantecmd
+            // 
+            this.estantecmd.Location = new System.Drawing.Point(613, 381);
+            this.estantecmd.Name = "estantecmd";
+            this.estantecmd.Size = new System.Drawing.Size(261, 22);
+            this.estantecmd.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 499);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 16);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Precio";
+            // 
+            // txt_cantidad
+            // 
+            this.txt_cantidad.Location = new System.Drawing.Point(12, 470);
+            this.txt_cantidad.Name = "txt_cantidad";
+            this.txt_cantidad.Size = new System.Drawing.Size(261, 22);
+            this.txt_cantidad.TabIndex = 21;
+            this.txt_cantidad.TextChanged += new System.EventHandler(this.txt_cantidad_TextChanged);
+            // 
+            // txt_precio
+            // 
+            this.txt_precio.Location = new System.Drawing.Point(15, 518);
+            this.txt_precio.Name = "txt_precio";
+            this.txt_precio.Size = new System.Drawing.Size(258, 22);
+            this.txt_precio.TabIndex = 22;
             // 
             // Form_Añadir_Productos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkCyan;
-            this.ClientSize = new System.Drawing.Size(922, 567);
+            this.ClientSize = new System.Drawing.Size(904, 567);
+            this.Controls.Add(this.txt_precio);
+            this.Controls.Add(this.txt_cantidad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.estantecmd);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -201,13 +243,13 @@
             this.Controls.Add(this.fechasalidacmd);
             this.Controls.Add(this.fechaingresocmd);
             this.Controls.Add(this.ubicacioncmd);
-            this.Controls.Add(this.cantidadcmd);
             this.Controls.Add(this.tipocmd);
             this.Controls.Add(this.nombrecmd);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form_Añadir_Productos";
             this.Text = "Añadir Productos";
+            this.Load += new System.EventHandler(this.Form_Añadir_Productos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -219,7 +261,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox nombrecmd;
         private System.Windows.Forms.TextBox tipocmd;
-        private System.Windows.Forms.TextBox cantidadcmd;
         private System.Windows.Forms.TextBox ubicacioncmd;
         private System.Windows.Forms.TextBox fechaingresocmd;
         private System.Windows.Forms.TextBox fechasalidacmd;
@@ -232,5 +273,10 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox estantecmd;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_cantidad;
+        private System.Windows.Forms.TextBox txt_precio;
     }
 }
--- Proyecto_AS/Proyecto_AS/Form_Añadir_Productos.cs
+++ Proyecto_AS/Proyecto_AS/Form_Añadir_Productos.cs
@@ -18,7 +18,7 @@ namespace Proyecto_AS
         //Creamos un string el cual contendra los datos para necesario para poder conectarse a la bd
         //static string inicio_sesion = "Server=LAPTOP-9H0B86NU ;Database=BD_AS ;User id=sa ;Password=697400naxo;";
         //static string inicio_sesion = "Server=PAOLO\\SQLEXPRESS ;Database=BD_AS ;User id=sa ;Password=12345678;";
-        static string inicio_sesion = "Server=LAPTOP-OBQGVQ1D ;Database=BD_AS ;User id=sa ;Password=2421;";
+        static string inicio_sesion = "Server=LAPTOP-OBQGVQ1D ;Database=BD_AS ;User id=sa ;Password=2024;";
         SqlConnection conectar = new SqlConnection(inicio_sesion); /*asignamos el comando para la conexion*/
         public Form_Añadir_Productos()
         {
@@ -37,15 +37,15 @@ namespace Proyecto_AS
 
         public void insertar()
         {
-            if (nombrecmd.Text != "" && tipocmd.Text != "" && cantidadcmd.Text != "" && ubicacioncmd.Text != "" &&
-    fechaingresocmd.Text != "" && fechasalidacmd.Text != "" && estadocmd.Text != "")
+            if (nombrecmd.Text != "" && tipocmd.Text != "" && txt_cantidad.Text != "" && txt_precio.Text != "" && ubicacioncmd.Text != "" &&
+                fechaingresocmd.Text != "" && fechasalidacmd.Text != "" && estadocmd.Text != "")
             {
-                string cmd = "INSERT INTO PRODUCTO (Nombre, Tipo, Cantidad, Ubicacion, FechaIngreso, FechaSalidad, Estado) " +
-                     "VALUES ('" + nombrecmd.Text + "','" + tipocmd.Text + "','" + cantidadcmd.Text + "','" + ubicacioncmd.Text + "','" + fechaingresocmd.Text + "','" + fechasalidacmd.Text + "','" + estadocmd.Text + "')";
-                SqlCommand lol = new SqlCommand(cmd, conectar);
-                SqlDataAdapter data = new SqlDataAdapter(lol);
-                DataTable red = new DataTable();
-                data.Fill(red);
+                string cmd = "INSERT INTO PRODUCTO (Nombre, Tipo, Cantidad, Precio, Ubicacion, FechaIngreso, FechaSalida, Estado, NivelEstante) " +
+                     "VALUES ('" + nombrecmd.Text + "','" + tipocmd.Text + "', '" + txt_cantidad.Text + "','" + txt_precio.Text  + "','" + ubicacioncmd.Text + "','" + fechaingresocmd.Text + "','" + fechasalidacmd.Text + "','" + estadocmd.Text + "', '" + estantecmd.Text + "')";
+                SqlCommand sqlCommand = new SqlCommand(cmd, conectar);
+                conectar.Open();
+                sqlCommand.ExecuteNonQuery();
+                conectar.Close();
 
                 MessageBox.Show("Se ha registrado correctamente");
             }
@@ -84,7 +84,7 @@ namespace Proyecto_AS
 
         public void Cantidad()
         {
-            conectar.Open();
+             conectar.Open();
             SqlCommand lol = new SqlCommand("select Cantidad from PRODUCTO", conectar);
             SqlDataAdapter data = new SqlDataAdapter(lol);
             DataTable red = new DataTable();
@@ -120,6 +120,7 @@ namespace Proyecto_AS
             rod["Caducidad"] = "";
             red.Rows.InsertAt(rod, 0);
         }
+
 
         public void Ubicacion()
         {
@@ -172,6 +173,19 @@ namespace Proyecto_AS
             rod["Estado"] = "";
             red.Rows.InsertAt(rod, 0);
         }
+
+        public void NivelEstante()
+        {
+            conectar.Open();
+            SqlCommand lol = new SqlCommand("select NivelEstante from PRODUCTO", conectar);
+            SqlDataAdapter data = new SqlDataAdapter(lol);
+            DataTable red = new DataTable();
+            data.Fill(red);
+            conectar.Close();
+            DataRow rod = red.NewRow();
+            rod["NivelEstante"] = "";
+            red.Rows.InsertAt(rod, 0);
+        }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             consultar();
@@ -184,11 +198,22 @@ namespace Proyecto_AS
             FechaIngreso();
             FechaSalida();
             Estado();
+            NivelEstante();
         }
 
         private void btnañadir_Click(object sender, EventArgs e)
         {
             insertar();
+        }
+
+        private void Form_Añadir_Productos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_cantidad_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
