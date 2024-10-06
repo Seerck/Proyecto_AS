318443b5c12b4af8af5c997f87d4c1d94e84ec8e
mayerk2 <mayerk.paolo@gmail.com>
2024-10-06 20:21:45 -0300

    formularios terminados, ver productos y usuarios

--- Proyecto_AS/Proyecto_AS/Form_Ver_Producto.Designer.cs
+++ Proyecto_AS/Proyecto_AS/Form_Ver_Producto.Designer.cs
@@ -45,19 +45,21 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateGray;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.GridColor = System.Drawing.Color.DarkSlateGray;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 11);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 9);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(812, 335);
+            this.dataGridView1.Size = new System.Drawing.Size(609, 272);
             this.dataGridView1.TabIndex = 1;
             // 
             // BtnBuscar
             // 
-            this.BtnBuscar.Location = new System.Drawing.Point(478, 389);
+            this.BtnBuscar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.BtnBuscar.Location = new System.Drawing.Point(428, 366);
+            this.BtnBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnBuscar.Name = "BtnBuscar";
-            this.BtnBuscar.Size = new System.Drawing.Size(192, 98);
+            this.BtnBuscar.Size = new System.Drawing.Size(117, 34);
             this.BtnBuscar.TabIndex = 2;
             this.BtnBuscar.Text = "Buscar";
             this.BtnBuscar.UseVisualStyleBackColor = true;
@@ -66,19 +68,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 397);
+            this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(18, 313);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.Size = new System.Drawing.Size(130, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Nombre Producto";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // TxtNombre
             // 
-            this.TxtNombre.Location = new System.Drawing.Point(150, 394);
+            this.TxtNombre.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.TxtNombre.Location = new System.Drawing.Point(188, 310);
+            this.TxtNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TxtNombre.MaxLength = 25;
             this.TxtNombre.Name = "TxtNombre";
-            this.TxtNombre.Size = new System.Drawing.Size(257, 22);
+            this.TxtNombre.Size = new System.Drawing.Size(197, 23);
             this.TxtNombre.TabIndex = 5;
             this.TxtNombre.TextChanged += new System.EventHandler(this.TxtNombre_TextChanged);
             this.TxtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNombre_KeyPress);
@@ -86,24 +92,27 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 466);
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(18, 375);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.Size = new System.Drawing.Size(105, 16);
             this.label3.TabIndex = 7;
             this.label3.Text = "Tipo Producto";
             // 
             // TxtTipo
             // 
+            this.TxtTipo.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
             this.TxtTipo.FormattingEnabled = true;
             this.TxtTipo.Items.AddRange(new object[] {
             "Cosmetico",
             "Ropa",
             "Electronico",
             "Calzado"});
-            this.TxtTipo.Location = new System.Drawing.Point(150, 463);
-            this.TxtTipo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtTipo.Location = new System.Drawing.Point(188, 372);
+            this.TxtTipo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TxtTipo.Name = "TxtTipo";
-            this.TxtTipo.Size = new System.Drawing.Size(257, 24);
+            this.TxtTipo.Size = new System.Drawing.Size(197, 24);
             this.TxtTipo.TabIndex = 25;
             this.TxtTipo.Text = "Seleccionar";
             this.TxtTipo.SelectedIndexChanged += new System.EventHandler(this.TxtTipo_SelectedIndexChanged);
@@ -111,10 +120,10 @@
             // 
             // Form_Ver_Producto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkCyan;
-            this.ClientSize = new System.Drawing.Size(832, 567);
+            this.ClientSize = new System.Drawing.Size(624, 461);
             this.Controls.Add(this.TxtTipo);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.TxtNombre);
@@ -122,7 +131,7 @@
             this.Controls.Add(this.BtnBuscar);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_Ver_Producto";
             this.Load += new System.EventHandler(this.Form_Ver_Producto_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- Proyecto_AS/Proyecto_AS/Form_Ver_Usuario.Designer.cs
+++ Proyecto_AS/Proyecto_AS/Form_Ver_Usuario.Designer.cs
@@ -42,12 +42,12 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateGray;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.GridColor = System.Drawing.Color.DarkSlateGray;
-            this.dataGridView1.Location = new System.Drawing.Point(11, 9);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(8, 7);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(812, 335);
+            this.dataGridView1.Size = new System.Drawing.Size(609, 272);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -55,34 +55,35 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(40, 441);
+            this.label5.Location = new System.Drawing.Point(30, 372);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(115, 19);
+            this.label5.Size = new System.Drawing.Size(99, 16);
             this.label5.TabIndex = 22;
             this.label5.Text = "Tipo Usuario:";
             // 
             // CBBTipoU
             // 
+            this.CBBTipoU.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
             this.CBBTipoU.FormattingEnabled = true;
             this.CBBTipoU.Items.AddRange(new object[] {
             "usuario",
             "super-usuario",
             "administrador"});
-            this.CBBTipoU.Location = new System.Drawing.Point(224, 436);
-            this.CBBTipoU.Margin = new System.Windows.Forms.Padding(4);
+            this.CBBTipoU.Location = new System.Drawing.Point(195, 369);
             this.CBBTipoU.Name = "CBBTipoU";
-            this.CBBTipoU.Size = new System.Drawing.Size(205, 24);
+            this.CBBTipoU.Size = new System.Drawing.Size(185, 24);
             this.CBBTipoU.TabIndex = 21;
             this.CBBTipoU.Text = "Seleccionar";
             this.CBBTipoU.SelectedIndexChanged += new System.EventHandler(this.cmd_tipo_SelectedIndexChanged);
             // 
             // TxtNombreU
             // 
-            this.TxtNombreU.Location = new System.Drawing.Point(224, 388);
-            this.TxtNombreU.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtNombreU.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.TxtNombreU.Location = new System.Drawing.Point(195, 315);
             this.TxtNombreU.MaxLength = 25;
             this.TxtNombreU.Name = "TxtNombreU";
-            this.TxtNombreU.Size = new System.Drawing.Size(205, 22);
+            this.TxtNombreU.Size = new System.Drawing.Size(185, 23);
             this.TxtNombreU.TabIndex = 20;
             this.TxtNombreU.TextChanged += new System.EventHandler(this.txt_nombre_TextChanged);
             this.TxtNombreU.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNombreU_Keypress);
@@ -91,18 +92,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 388);
+            this.label1.Location = new System.Drawing.Point(30, 315);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 19);
+            this.label1.Size = new System.Drawing.Size(124, 16);
             this.label1.TabIndex = 19;
             this.label1.Text = "Nombre Usuario:";
             // 
             // BtnBuscarU
             // 
-            this.BtnBuscarU.Location = new System.Drawing.Point(534, 388);
-            this.BtnBuscarU.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnBuscarU.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.BtnBuscarU.Location = new System.Drawing.Point(416, 363);
             this.BtnBuscarU.Name = "BtnBuscarU";
-            this.BtnBuscarU.Size = new System.Drawing.Size(191, 90);
+            this.BtnBuscarU.Size = new System.Drawing.Size(110, 35);
             this.BtnBuscarU.TabIndex = 23;
             this.BtnBuscarU.Text = "Buscar";
             this.BtnBuscarU.UseVisualStyleBackColor = true;
@@ -110,10 +112,10 @@
             // 
             // Form_Ver_Usuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkCyan;
-            this.ClientSize = new System.Drawing.Size(832, 567);
+            this.ClientSize = new System.Drawing.Size(624, 461);
             this.Controls.Add(this.BtnBuscarU);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.CBBTipoU);
@@ -121,7 +123,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_Ver_Usuario";
             this.Text = "Mostrar Usuario";
             this.Load += new System.EventHandler(this.Form_Ver_Usuario_Load);
